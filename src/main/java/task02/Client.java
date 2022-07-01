c7b9c9ce62e6e90692f076f17de6c5e9c466e0d3
Khaizad52 <1khairulh@gmail.com>
2022-07-01 15:57:02 +0800

    3

--- src/main/java/task02/Client.java
+++ src/main/java/task02/Client.java
@@ -1,17 +1,14 @@
 package task02;
 
-import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
-import javax.swing.plaf.basic.BasicBorders.SplitPaneBorder;
+import java.util.LinkedList;
 
 public class Client {
 
@@ -28,22 +25,34 @@ public class Client {
             InputStream is = socket.getInputStream();
             ObjectInputStream ois = new ObjectInputStream(is);
         
-            ois.readUTF();
             try{
                 response = ois.readUTF();
-            }catch(EOFException e){
 
-            }
+                if(response != null){
+                    String[] value = response.split(" ");
+                    String requestID = value[0];
+                    String num = value[1];
+                    System.out.println(requestID);
+                    System.out.println(num);
 
-            if(response != null){
-                String[] value = response.split(" ");
-                String requestID = value[0];
-                String num = value[1];
-                System.out.println(requestID);
-                System.out.println(num);
-            }
+                    String[] numList = num.split(",");
+                    LinkedList<Integer> intList = new LinkedList<>();
 
-            
+                    int total =0;
+                    float average = 0;
+                    for(String combine: numList){
+                        try{
+                            int numbers = Integer.parseInt(combine);
+                            total = total + numbers;
+                            intList.add(numbers);
+
+                        }catch(NumberFormatException ex){
+                            ex.printStackTrace();
+                        }
+                    average =(float)total/intList.size();
+                    System.out.println(average);
+                    }
+                }
 
         }catch(UnknownHostException e){
             System.out.println("Server not found: " + e.getMessage());
@@ -51,5 +60,7 @@ public class Client {
             System.out.println("I/O error: " + e.getMessage());
         }
     }
+    }
 }
+
     
